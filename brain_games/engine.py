8942e020e674ed5e27bc95d20776af11e484cb22
Артур Исламов <arturislamov91@yandex.ru>
2023-12-13 20:17:15 +0500

    corrected errors after re-review

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,18 +8,18 @@ NUMBER_OF_ATTEMPTS = 3
 
 
 def run_game(
-    GAME_RULES: str, get_question_and_answer: Callable[[], Tuple[str, str]]
+    game_rules: str, get_question_and_answer: Callable[[], Tuple[str, str]]
 ) -> None:
     print(GAME_MESSAGE)
     name = welcome_user()
-    print(GAME_RULES)
+    print(game_rules)
     for _ in range(NUMBER_OF_ATTEMPTS):
         content_of_question, correct_answer = get_question_and_answer()
         print(f'Question: {content_of_question}')
 
-        user_answer = prompt.string('Your answer: ').lower()
+        user_answer = prompt.string('Your answer: ')
 
-        if user_answer != correct_answer:
+        if user_answer.lower() != correct_answer:
             print(
                 f"'{user_answer}' is wrong answer ;(. "
                 f"Correct answer was '{correct_answer}'."
